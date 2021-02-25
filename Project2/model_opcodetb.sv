3d9efc491227eea4c491c88adb97d98aae88aef6
Bram Fouts <43616716+abfouts@users.noreply.github.com>
2021-02-24 23:53:14 -0800

    Update model_opcodetb.sv

--- Project2/model_opcodetb.sv
+++ Project2/model_opcodetb.sv
@@ -27,51 +27,51 @@ module Problem3_TB;
 		write_file = $fopen("log.txt", "a");
 		$fwrite(write_file, "\n\nPROBLEM 3\n\n");
 
-		inp <= $random;
-		opcode <= 101; #10;
+		inp = $random;
+		opcode = 101; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 		
 
 		//test begin
-		opcode <= 000; #10;
+		opcode = 000; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 
-		inp <= $random;
-		opcode <= 001; #10;
+		inp = $random;
+		opcode = 001; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 
-		opcode <= 010; #10;
+		opcode = 010; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 
-		opcode <= 011; #10;
+		opcode = 011; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 
-		opcode <= 100; #10;
+		opcode = 100; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 
-		opcode <= 101; #10;
+		opcode = 101; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 
-		opcode <= 110; #10;
+		opcode = 110; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 
-		opcode <= 111; #10;
+		opcode = 111; #10;
 		$fwrite(write_file, "time=",$time,, "INPUT : IN = %b, OP = %b\n", inp, opcode);
 		$fwrite(write_file, "time=",$time,, "OUTPUT = %b \n", outp);
 		
 	end
 
 	always begin
-		clk <= 1; #5;
-		clk <= 0; #5;
+		clk = 1; #5;
+		clk = 0; #5;
 	end
 
 endmodule
