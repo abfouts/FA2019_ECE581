75e21064a97e81a2fa4d64445e837882fa637334
Bram Fouts <43616716+abfouts@users.noreply.github.com>
2021-02-24 23:46:25 -0800

    Update 8to1mux_1.sv

--- Project2/8to1mux_1.sv
+++ Project2/8to1mux_1.sv
@@ -19,20 +19,19 @@ module Problem4
 
 	always_comb begin
 		case (op)		//Mux
-			3'b000: temp <= in[0];
-			3'b001: temp <= in[1];
-			3'b010: temp <= in[2];
-			3'b011: temp <= in[3];
-			3'b100: temp <= in[4];
-			3'b101: temp <= in[5];
-			3'b110: temp <= in[6];
-			3'b111: temp <= in[7];
+			3'b000: temp = in[0];
+			3'b001: temp = in[1];
+			3'b010: temp = in[2];
+			3'b011: temp = in[3];
+			3'b100: temp = in[4];
+			3'b101: temp = in[5];
+			3'b110: temp = in[6];
+			3'b111: temp = in[7];
 		endcase	
 	end
 
 
 	always_ff @(posedge clk) begin		
-		
 		out <= temp;
 	end
 
